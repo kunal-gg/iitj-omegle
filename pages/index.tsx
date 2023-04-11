c4fef7ac539db279855ae0c4f5731383625c523e
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-04-11 20:36:26 +0530

    added final touches

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,13 +12,14 @@ export default function Home() {
       </section>
       <hr className='w-11/12 m-auto'/>
       <section className='flex w-screen h-full'>
-        <div className='w-2/3 h-full flex flex-col'>
-          <input type="text" className='bg-primary border-secondary border-4 w-1/2 h-16 px-5 text-primary-text mx-10 rounded-xl' placeholder='Enter your interests here:'/>
-          <div className='w-1/3 flex flex-row'>
-          <button className='w-full text-[#add7ff] bg-secondary px-10 py-5 rounded-xl'>Start Chat Room</button>
-          <button className='w-full text-[#add7ff] bg-secondary px-10 py-5 rounded-xl'>Find Match</button>
+        <div className='w-2/3 h-full flex justify-start items-center  ml-10'>
+        <div className='flex flex-col w-1/2 '>
+          <input type="text" className='my-10 bg-primary border-secondary border-4 w-full h-16 px-5 text-primary-text rounded-xl ' placeholder='Enter your interests here:'/>
+          <div className='w-full flex flex-row mb-10'>
+          <button className='basis-1/2 mx-3  text-[#add7ff] bg-secondary px-10 py-5 rounded-xl'>Start Chat Room</button>
+          <button className='basis-1/2 mx-3 text-[#add7ff] bg-secondary px-10 py-5 rounded-xl'>Find Match</button>
           </div>
-
+        </div>
         </div>
         <div className='border-l border-white w-1/3 h-full flex flex-col justify-evenly items-center'>
           <h1 className='text-[#add7ff] text-2xl self-start px-10 font-bold'>Chat Rooms</h1>
